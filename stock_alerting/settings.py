cd0a4448478a59035752363d9915790fff784f57
mohammedhisham1 <mohamad92moh@gmail.com>
2025-08-09 22:30:50 +0300

    Update .gitignore to exclude frontend/node_modules and clean up CORS settings in settings.py

--- stock_alerting/settings.py
+++ stock_alerting/settings.py
@@ -183,10 +183,9 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
     "http://localhost:8080",
     "http://127.0.0.1:8080",
+    
 ]
 
 CORS_ALLOW_CREDENTIALS = True
